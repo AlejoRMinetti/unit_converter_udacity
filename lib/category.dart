ef42652ae395b28da7565c2283aa85d5adb3289c
alejorminetti <alejorm@gmail.com>
2021-03-20 18:30:33 -0300

    added Category widget

--- lib/category.dart
+++ lib/category.dart
@@ -6,17 +6,23 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+const double _category_height = 100;
+const double _border_radius = _category_height / 2;
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
+  final IconData icono;
+  final String nombre;
+  final Color colorido;
+
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category(this.nombre, this.colorido, this.icono);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -27,7 +33,35 @@ class Category extends StatelessWidget {
   // Theme ancestor in the tree. Below, we obtain the display1 text theme.
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Material(
+      child: Container(
+        height: _category_height,
+        padding: EdgeInsets.all(8),
+        child: InkWell(
+          onTap: () {
+            print('Me toquetearon!');
+          },
+          splashColor: colorido,
+          highlightColor: colorido,
+          borderRadius: BorderRadius.circular(_border_radius),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Icon(
+                  icono,
+                  size: 60,
+                ),
+              ),
+              Center(
+                  child: Text(
+                nombre,
+                style: TextStyle(fontSize: 24),
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
